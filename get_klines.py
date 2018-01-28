3533c5906b717ae2e333a6f0bce0c65a734e538e
georg_un <onkovic@gmail.com>
2018-01-28 23:14:02 +0100

    Execute downloads in loop

--- get_klines.py
+++ get_klines.py
@@ -7,6 +7,22 @@ import sqlite3
 import datetime as d
 
 
+# load functions
+def getlist(option, sep=',', chars=None):
+    """Return a list from a ConfigParser option. By default,
+       split on a comma and strip whitespaces."""
+    return [ chunk.strip(chars) for chunk in option.split(sep) ]
+
+def api_time_format(milliseconds):
+    """Convert a unix timestamp in milliseconds to the
+    date-format required by binance.client."""
+    timestamp = d.datetime.fromtimestamp(milliseconds/1000)
+    time_str = d.date.strftime(timestamp, '%B %d, %Y %H:%M:%S')
+    return time_str
+
+
+# READ FILES
+
 # read credentials
 configParser = configparser.ConfigParser()
 configParser.read(r'credentials/API-key')
@@ -14,118 +30,104 @@ configParser.read(r'credentials/API-key')
 api_key = configParser.get('credentials', 'api_key')
 api_sec = configParser.get('credentials', 'api_secret')
 
+
 # read config
 configParser.read(r'config.txt')
+
 db_path = configParser.get('config', 'db_path')
+symbols = getlist(configParser.get('symbols', 'symbol_list'))
+intervals = getlist(configParser.get('intervals', 'interval_list'))
+time_start = configParser.get('time', 'time_start')
+time_end = configParser.get('time', 'time_end')
 
 
+# create timestamps for beginning and now
+if time_start == 'beginning':
+    time_start = 946684800000   # January 1, 2000
 
-# TEMPORARY
-symbol = 'LTCETH'
-interval = '15m'
-time_start = 'October 14, 2004 CET'
-time_end = d.datetime.now().utcnow().timestamp() * 1000
-
-
-
-def api_time_format(milliseconds):
-    timestamp = d.datetime.fromtimestamp(milliseconds/1000)
-    time_str = d.date.strftime(timestamp, '%B %d, %Y %H:%M:%S')
-    return time_str
+if time_end == 'now':
+    time_end = d.datetime.now().timestamp() * 1000
 
 
+# SET UP DATABASE CONNECTION AND BINANCE CLIENT
 
 # connect to database
 db_con = sqlite3.connect(db_path)
 
-# set up client
+# set up binance client
 client = Client(api_key, api_sec)
 
 
+# Loop over every symbol and over every time-interval. Create for each combination a table in the database,
+# if it does not exist yet and fill it with the respective values
+for symbol in symbols:
+    for interval in intervals:
+        # create table in database if it does not exist yet
+        with db_con:
+            cur = db_con.cursor()
+            cur.execute('CREATE TABLE IF NOT EXISTS {}_{}('.format(symbol, interval) +
+                        't_open DATETIME, ' +
+                        'open FLOAT, ' +
+                        'high FLOAT, ' +
+                        'low FLOAT, ' +
+                        'close FLOAT, ' +
+                        'vol FLOAT, ' +
+                        't_close DATETIME, ' +
+                        'u_vol FLOAT, ' +
+                        'no_trds INT, ' +
+                        'tbBav FLOAT, ' +
+                        'tbQav FLOAT)')
+
+        # test if there are already entries in the table
+        with db_con:
+            cur = db_con.cursor()
+            cur.execute('SELECT COUNT(*) FROM {}_{}'.format(symbol, interval))
+            table_empty = (cur.fetchone()[0] == 0)
+
+        # if there were no entries in the table, get the first time the symbol was traded on binance.com
+        if table_empty == True:
+            next_db_entry = client.get_historical_klines(symbol = symbol,
+                                                         interval = interval,
+                                                         start_str = api_time_format(time_start))[0][0]
+        # if the table did exist, get the latest entry in the database for the current symbol
+        elif table_empty == False:
+            with db_con:
+                cur = db_con.cursor()
+                next_db_entry = cur.execute('SELECT MAX(t_open) FROM {}_{}'.format(symbol, interval)).fetchone()[0]
+                next_db_entry += 900000
+
+        # request data on binance, until the database is up to date
+        while next_db_entry <= time_end:
+            # download data
+            output = client.get_historical_klines(symbol = symbol,
+                                                  interval = interval,
+                                                  start_str = api_time_format(next_db_entry))
+            # write downloaded data to database
+            with db_con:
+                cur = db_con.cursor()
+                for x in range(0, len(output)):
+                    cur.execute('INSERT INTO {}_{} '.format(symbol, interval) +
+                                'VALUES({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})'.format(output[x][0],
+                                                                                            output[x][1],
+                                                                                            output[x][2],
+                                                                                            output[x][3],
+                                                                                            output[x][4],
+                                                                                            output[x][5],
+                                                                                            output[x][6],
+                                                                                            output[x][7],
+                                                                                            output[x][8],
+                                                                                            output[x][9],
+                                                                                            output[x][10]))
+            # get latest database entry
+            with db_con:
+                cur = db_con.cursor()
+                next_db_entry = cur.execute('SELECT MAX(t_open) FROM {}_{}'.format(symbol, interval)).fetchone()[0]
+                next_db_entry += 900000
+            print(api_time_format(next_db_entry))
 
 
-
-
-# create table if it does not exist yet
-with db_con:
-    cur = db_con.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS {}_{}('.format(symbol, interval) +
-                't_open DATETIME, ' +
-                'open FLOAT, ' +
-                'high FLOAT, ' +
-                'low FLOAT, ' +
-                'close FLOAT, ' +
-                'vol FLOAT, ' +
-                't_close DATETIME, ' +
-                'u_vol FLOAT, ' +
-                'no_trds INT, ' +
-                'tbBav FLOAT, ' +
-                'tbQav FLOAT)')
-
-
-# test if there are already entries in the table
-with db_con:
-    cur = db_con.cursor()
-    cur.execute('SELECT COUNT(*) FROM {}_{}'.format(symbol, interval))
-    table_empty = (cur.fetchone()[0] == 0)
-
-
-# if there were no entries in the table, get the first time the symbol was traded on binance.com
-if table_empty == True:
-    start_time = client.get_historical_klines(symbol=symbol,
-                                              interval=interval,
-                                              start_str=time_start)[0][0]
-# if the table did exist, get the latest entry in the database for the current symbol
-elif table_empty == False:
-    with db_con:
-        cur = db_con.cursor()
-        start_time = cur.execute('SELECT MAX(t_open) FROM {}_{}'.format(symbol, interval)).fetchone()[0]
-
-
-
-
-
-
-
-
-# most_recent = start_time
-# so lange most_recent < als end_time, hol daten und schreibe in datenbank
-# errechne neuen most_recent
-
-most_recent = start_time
-
-while most_recent <= time_end:
-    # download data
-    output = client.get_historical_klines(symbol = symbol,
-                                          interval = interval,
-                                          start_str = api_time_format(most_recent))
-    # write downloaded data to database
-    with db_con:
-        cur = db_con.cursor()
-        for x in range(0, len(output)):
-            cur.execute('INSERT INTO {}_{} '.format(symbol, interval) +
-                        'VALUES({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})'.format(output[x][0],
-                                                                                    output[x][1],
-                                                                                    output[x][2],
-                                                                                    output[x][3],
-                                                                                    output[x][4],
-                                                                                    output[x][5],
-                                                                                    output[x][6],
-                                                                                    output[x][7],
-                                                                                    output[x][8],
-                                                                                    output[x][9],
-                                                                                    output[x][10]))
-    # get latest database entry
-    with db_con:
-        cur = db_con.cursor()
-        most_recent = cur.execute('SELECT MAX(t_open) FROM {}_{}'.format(symbol, interval)).fetchone()[0]
-
-
-
-
-
-
-#import datetime
-#dates = [lst[0] for lst in output]
-#print([datetime.datetime.fromtimestamp(x/1000) for x in dates])
-
+        # wait, until there were on average 1200 requests per minute
+        #import time
+        #import math
+        #minutes = math.ceil(len(output) / 1200)
+        #time.sleep(minutes * 60)
