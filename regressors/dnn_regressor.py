537aca6c71851114d8bc1b8aff30ac64a1d1f80d
georg_un <onkovic@gmail.com>
2018-02-15 00:37:33 +0100

    Train deep neural network

---
+++ regressors/dnn_regressor.py
@@ -0,0 +1,85 @@
+import configparser
+import sqlite3
+
+import numpy as np
+import tensorflow as tf
+
+from sklearn.preprocessing import MinMaxScaler
+
+from general.general_modules import getlist
+from regressors.modules.regression_modules import transform_fn
+from regressors.modules.regression_modules import train_input_fn
+from regressors.modules.regression_modules import get_feature_columns
+
+
+# READ CONFIG
+
+# set up config parser
+configParser = configparser.ConfigParser()
+
+# read config-file
+configParser.read(r'config.txt')
+
+db_path = configParser.get('config', 'db_path')
+symbols = getlist(configParser.get('symbols', 'symbol_list'))
+intervals = getlist(configParser.get('intervals', 'interval_list'))
+
+BATCH_SIZE = 5
+F_HORIZON = 1
+
+
+# SET UP DATABASE CONNECTION
+
+# connect to database
+db_con = sqlite3.connect(db_path)
+
+with db_con:
+    for symbol in symbols:
+        cur = db_con.cursor()
+        cur.execute('SELECT {0}_15m.close, {0}_15m_feat.* FROM {0}_15m_feat JOIN {0}_15m ON {0}_15m.t_open={0}_15m_feat.t_open'.format(symbol))
+        data = np.array(cur.fetchall()).astype('float32')
+
+data = data[100:, :]
+
+
+# PREPARE DATA
+
+# split data into test and training
+test_split = int(len(data) * 0.75)
+
+# create training data
+train_X = data[:test_split, 2:]
+train_y = data[F_HORIZON:F_HORIZON+test_split, 0]
+
+# create test data
+test_X = data[test_split+1: len(data)-F_HORIZON-1, 2:]
+test_y = data[test_split+1+F_HORIZON: len(data)-1, 0]
+
+
+# scale data
+scaler = MinMaxScaler()
+train_X = scaler.fit_transform(train_X)
+test_X = scaler.fit_transform(test_X)
+
+# transform data
+train_X, train_y = transform_fn(train_X, train_y)
+test_X, test_y = transform_fn(test_X, test_y)
+
+
+# create feature column tensor
+feature_columns = get_feature_columns(train_X)
+
+
+# MODEL
+
+# define regressor
+model = tf.estimator.DNNRegressor(hidden_units=[256, 128, 64, 32, 16],
+                                  feature_columns=feature_columns,
+                                  model_dir='/home/georg/test/')
+
+
+# train model
+model.train(input_fn=lambda: train_input_fn(train_X, train_y, 50), steps=1500)
+
+# evaluate model
+model.evaluate(input_fn=lambda: train_input_fn(test_X, test_y, 50))
