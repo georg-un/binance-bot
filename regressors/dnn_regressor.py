74203280304d9984054e027cb29c55c825e0bf00
georg_un <onkovic@gmail.com>
2018-02-19 01:10:18 +0100

    Add scaling & evaluation metrics

--- regressors/dnn_regressor.py
+++ regressors/dnn_regressor.py
@@ -5,11 +5,15 @@ import numpy as np
 import tensorflow as tf
 
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import explained_variance_score
+from sklearn.metrics import r2_score
 
 from general.general_modules import getlist
 from regressors.modules.regression_modules import transform_fn
 from regressors.modules.regression_modules import train_input_fn
-from regressors.modules.regression_modules import get_feature_columns
+from regressors.modules.regression_modules import get_default_feature_columns
+from regressors.modules.regression_modules import combine_symbols
 
 
 # READ CONFIG
@@ -26,60 +30,140 @@ intervals = getlist(configParser.get('intervals', 'interval_list'))
 
 BATCH_SIZE = 5
 F_HORIZON = 1
+TEST_RATIO = 0.75
 
 
-# SET UP DATABASE CONNECTION
+# DEFINE MODEL
 
-# connect to database
-db_con = sqlite3.connect(db_path)
-
-with db_con:
-    for symbol in symbols:
-        cur = db_con.cursor()
-        cur.execute('SELECT {0}_15m.close, {0}_15m_feat.* FROM {0}_15m_feat JOIN {0}_15m ON {0}_15m.t_open={0}_15m_feat.t_open'.format(symbol))
-        data = np.array(cur.fetchall()).astype('float32')
-
-data = data[100:, :]
+# define regressor
+regressor = tf.estimator.DNNRegressor(hidden_units=[256, 128, 64, 32, 16],
+                                      feature_columns=get_default_feature_columns(),
+                                      model_dir='/home/georg/test/dnn')
 
 
 # PREPARE DATA
 
-# split data into test and training
-test_split = int(len(data) * 0.75)
+# connect to database
+db_con = sqlite3.connect(db_path)
 
-# create training data
-train_X = data[:test_split, 2:]
-train_y = data[F_HORIZON:F_HORIZON+test_split, 0]
+# set up dictionaries
+training_dict = {}
+test_dict = {}
 
-# create test data
-test_X = data[test_split+1: len(data)-F_HORIZON-1, 2:]
-test_y = data[test_split+1+F_HORIZON: len(data)-1, 0]
+# download feature and price data for each symbol, split in training and test sets and write them to
+# their respective dictionaries
+with db_con:
+    for symbol in symbols:
+
+        # select all features and the closing price
+        cur = db_con.cursor()
+        cur.execute('SELECT {0}_15m.close, {0}_15m_feat.* FROM {0}_15m_feat JOIN {0}_15m ON {0}_15m.t_open={0}_15m_feat.t_open'.format(symbol))
+        data = np.array(cur.fetchall()).astype('float32')
+
+        # drop the first n observations (longest period length in feature calculation)
+        data = data[100:, :]
+
+        # split data into test and training
+        test_split = int(len(data) * TEST_RATIO)
+
+        # create training data
+        train_X = data[:test_split, 2:]
+        train_y = data[F_HORIZON:F_HORIZON + test_split, 0]
+
+        # create test data
+        test_X = data[test_split + 1: len(data) - F_HORIZON - 1, 2:]
+        test_y = data[test_split + 1 + F_HORIZON: len(data) - 1, 0]
+
+        # scale data
+        scaler = MinMaxScaler()
+
+        train_X = scaler.fit_transform(train_X)
+        train_y = scaler.fit_transform(train_y.reshape(-1, 1))
+        #train_y = train_y.reshape(-1, 1)
+
+        test_X = scaler.fit_transform(test_X)
+        #test_y = scaler.fit_transform(test_y.reshape(-1, 1))
+        test_y = test_y.reshape(-1, 1)
+
+        # join training data and make dictionary
+        training_data = np.hstack((train_X, train_y))
+        training_dict[symbol] = training_data
+
+        # transform test data and make dictionary
+        test_X, test_y = transform_fn(test_X, test_y)
+        test_dict[symbol] = [test_X, test_y]
 
 
-# scale data
-scaler = MinMaxScaler()
-train_X = scaler.fit_transform(train_X)
-test_X = scaler.fit_transform(test_X)
+# combine training data for all symbols and shuffle
+training_data = combine_symbols(training_dict, chunksize=20, batchsize=5, shuffle=True)
 
-# transform data
+# split training data into features and targets
+training_data = np.hsplit(training_data, np.array((len(training_data[0])-1, len(training_data[0]))))
+train_X = training_data[0]
+train_y = training_data[1].flatten()
+
+# transform training data
 train_X, train_y = transform_fn(train_X, train_y)
-test_X, test_y = transform_fn(test_X, test_y)
 
 
-# create feature column tensor
-feature_columns = get_feature_columns(train_X)
-
-
-# MODEL
-
-# define regressor
-model = tf.estimator.DNNRegressor(hidden_units=[256, 128, 64, 32, 16],
-                                  feature_columns=feature_columns,
-                                  model_dir='/home/georg/test/dnn')
-
+# TRAIN MODEL
 
 # train model
-model.train(input_fn=lambda: train_input_fn(train_X, train_y, BATCH_SIZE), steps=int(len(train_y)/BATCH_SIZE))
+for _ in range(0, 5):
+    regressor.train(input_fn=lambda: train_input_fn(train_X, train_y, BATCH_SIZE), steps=int(len(train_y)/BATCH_SIZE))
 
-# evaluate model
-model.evaluate(input_fn=lambda: train_input_fn(test_X, test_y, BATCH_SIZE))
+
+# EVALUATE MODEL
+
+# use build in evaluation
+for symbol in symbols:
+    test_X = test_dict[symbol][0]
+    test_y = test_dict[symbol][1]
+
+    print('Evaluating {}'.format(symbol))
+
+    regressor.evaluate(input_fn=lambda: train_input_fn(test_X, scaler.fit_transform(test_y), BATCH_SIZE))
+
+
+# get predictions
+metrics = {'MAE': [],
+           'EVS': [],
+           'R2': []}
+
+for symbol in symbols:
+    test_X = test_dict[symbol][0]
+    test_y = test_dict[symbol][1]
+
+    # get predictions
+    predictions = list(regressor.predict(input_fn=lambda: train_input_fn(test_X, scaler.fit_transform(test_y), BATCH_SIZE)))
+    predict_y = [predictions[i]['predictions'] for i in range(0, len(predictions))]
+    predict_y = np.asarray(predict_y).reshape(-1, 1)
+
+    # transform scaled predictions back to normal
+    scaler.fit(test_y)
+    predict_y = scaler.inverse_transform(predict_y)
+
+    # calculate metrics
+    metrics['MAE'].append(
+        mean_absolute_error(test_y, predict_y))
+    metrics['EVS'].append(
+        explained_variance_score(test_y, predict_y))
+    metrics['R2'].append(
+        r2_score(test_y, predict_y))
+
+# print scores
+print('Explained Variance Score: Highest={}, Lowest={}, Mean={}'.format(
+    max(metrics['EVS']),
+    min(metrics['EVS']),
+    np.mean(metrics['EVS'])
+))
+print('Mean Absolute Error: Highest={}, Lowest={}, Mean={}'.format(
+    max(metrics['MAE']),
+    min(metrics['MAE']),
+    np.mean(metrics['MAE'])
+))
+print('R Squared: Highest={}, Lowest={}, Mean={}'.format(
+    max(metrics['R2']),
+    min(metrics['R2']),
+    np.mean(metrics['R2'])
+))
