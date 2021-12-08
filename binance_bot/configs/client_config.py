ab7e10c479104f4ae7c5ca48c11ef1ebdc352b97
Georg Unterholzner <georg.unterholzner.coding@gmail.com>
2021-12-08 23:23:24 +0100

    Add base symbol to config

--- binance_bot/configs/client_config.py
+++ binance_bot/configs/client_config.py
@@ -7,8 +7,9 @@ class ClientConfig:
     def __init__(self):
         self._configParser = configparser.ConfigParser()
         self._configParser.read(r'config/client-config.ini')
-        self.PAIRS = getlist(self._configParser.get('pairs', 'pair_list'))
         self.SYMBOLS = getlist(self._configParser.get('symbols', 'symbols'))
+        self.BASE_SYMBOL = self._configParser.get('symbols', 'base_symbol')
         self.INTERVALS = getlist(self._configParser.get('intervals', 'interval_list'))
         self.TIME_START = getlist(self._configParser.get('time', 'time_start'))
         self.TIME_END = getlist(self._configParser.get('time', 'time_end'))
+        self.PAIRS = [symbol + self.BASE_SYMBOL for symbol in self.SYMBOLS]
