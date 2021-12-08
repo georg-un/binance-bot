50f3cc513187f26f82c201b8f8bea302e257cde2
Georg Unterholzner <georg.unterholzner.coding@gmail.com>
2021-12-08 13:27:55 +0100

    Switch to named pandas objects for indicator

--- binance_bot/processing/indicator_functions.py
+++ binance_bot/processing/indicator_functions.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import talib
 
+from binance_bot.constants import Indicators
+
 """
 Note: price values have to be multiplied before calculations and divided afterwards due to a bug in talib occuring
 with small values (https://github.com/mrjbq7/ta-lib/issues/151)
@@ -24,72 +26,77 @@ def calc_bollinger_bands(
         bbands_lower: int,
         bbands_upper: int,
         bbands_matype: str
-) -> np.ndarray:
-    """Calculate Bollinger Bands. Return a 2d numpy array containing the three Bollinger Bands."""
+) -> pd.DataFrame:
+    """Calculate Bollinger Bands. Return a pandas dataframe containing the three Bollinger Bands."""
     bbands = talib.BBANDS(prices * 10000,
                           timeperiod=bbands_period,
                           nbdevup=bbands_upper,
                           nbdevdn=bbands_lower,
                           matype=bbands_matype)
-    # divide results by 10000
-    bband_up = bbands[0] / 10000
-    bband_mid = bbands[1] / 10000
-    bband_low = bbands[2] / 10000
-    return np.column_stack((bband_up, bband_mid, bband_low))
+    return pd.DataFrame({
+        Indicators.BOLL_UP: bbands[0] / 10000,
+        Indicators.BOLL_MID: bbands[1] / 10000,
+        Indicators.BOLL_LOW: bbands[2] / 10000
+    })
 
 
-def calc_ema(prices: np.ndarray, ema_period_short: int, ema_period_mid: int, ema_period_long: int) -> np.ndarray:
-    """Calculate the exponential moving average. Return a 2d numpy array containing the short, mid, and long EMA."""
-    ema_short = talib.EMA(prices, timeperiod=ema_period_short)
-    ema_mid = talib.EMA(prices, timeperiod=ema_period_mid)
-    ema_long = talib.EMA(prices, timeperiod=ema_period_long)
-    return np.column_stack((ema_short, ema_mid, ema_long))
+def calc_ema(prices: np.ndarray, ema_period_short: int, ema_period_mid: int, ema_period_long: int) -> pd.DataFrame:
+    """Calculate the exponential moving average. Return a pandas dataframe containing the short, mid, and long EMA."""
+    return pd.DataFrame({
+        Indicators.EMA_SHORT: talib.EMA(prices, timeperiod=ema_period_short),
+        Indicators.EMA_MID: talib.EMA(prices, timeperiod=ema_period_mid),
+        Indicators.EMA_LONG: talib.EMA(prices, timeperiod=ema_period_long)
+    })
 
 
-def calc_sma(prices: np.ndarray, sma_period_short: int, sma_period_mid: int, sma_period_long: int) -> np.ndarray:
-    """Calculate the simple moving average. Return a 2d numpy array containing the short, mid, and long SMA."""
-    sma_short = talib.SMA(prices, timeperiod=sma_period_short)
-    sma_mid = talib.SMA(prices, timeperiod=sma_period_mid)
-    sma_long = talib.SMA(prices, timeperiod=sma_period_long)
-    return np.column_stack((sma_short, sma_mid, sma_long))
+def calc_sma(prices: np.ndarray, sma_period_short: int, sma_period_mid: int, sma_period_long: int) -> pd.DataFrame:
+    """Calculate the simple moving average. Return a pandas dataframe containing the short, mid, and long SMA."""
+    return pd.DataFrame({
+        Indicators.SMA_SHORT: talib.SMA(prices, timeperiod=sma_period_short),
+        Indicators.SMA_MID: talib.SMA(prices, timeperiod=sma_period_mid),
+        Indicators.SMA_LONG: talib.SMA(prices, timeperiod=sma_period_long)
+    })
 
 
-def calc_typical_price(highs: np.ndarray, lows: np.ndarray, closes: np.ndarray) -> np.ndarray:
+def calc_typical_price(highs: np.ndarray, lows: np.ndarray, closes: np.ndarray) -> pd.Series:
     """Calculate (high + low + close) / 3 to get the typical price of each period.
     Return a 1d numpy array containing the typical price values."""
-    return (highs + lows + closes) / 3
+    return pd.Series((highs + lows + closes) / 3, name=Indicators.TYPICAL_PRICE)
 
 
 # MOMENTUM INDICATORS
 
-def calc_adx(hights: np.ndarray, lows: np.ndarray, closes: np.ndarray, adx_period: int) -> np.ndarray:
+def calc_adx(hights: np.ndarray, lows: np.ndarray, closes: np.ndarray, adx_period: int) -> pd.Series:
     """Calculate the ADX. Return a 1d numpy array containing the ADX-values."""
-    return talib.ADX(high=hights, low=lows, close=closes, timeperiod=adx_period)
+    return pd.Series(talib.ADX(high=hights, low=lows, close=closes, timeperiod=adx_period), name=Indicators.ADX)
 
 
-def calc_cci(highs: np.ndarray, lows: np.ndarray, closes: np.ndarray, cci_period: int) -> np.ndarray:
+def calc_cci(highs: np.ndarray, lows: np.ndarray, closes: np.ndarray, cci_period: int) -> pd.Series:
     """Calculate the CCI. Return a 1d numpy array containing the CCI-values."""
-    return talib.CCI(high=highs, low=lows, close=closes, timeperiod=cci_period)
+    return pd.Series(talib.CCI(high=highs, low=lows, close=closes, timeperiod=cci_period), name=Indicators.CCI)
 
 
-def calc_macd(prices: np.ndarray, macd_fastperiod: int, macd_slowperiod: int, macd_signalperiod: int) -> np.ndarray:
+def calc_macd(prices: np.ndarray, macd_fastperiod: int, macd_slowperiod: int, macd_signalperiod: int) -> pd.DataFrame:
     """Calculate the MACD. Return a 2d numpy array containing the MACD, the Signal, and the histogram."""
     macd = talib.MACD(prices * 10000,
                       fastperiod=macd_fastperiod,
                       slowperiod=macd_slowperiod,
                       signalperiod=macd_signalperiod)
+    return pd.DataFrame({
+        Indicators.MACD: macd[0],
+        Indicators.MACD_SIGNAL: macd[1],
+        Indicators.MACD_HIST: macd[2]
+    })
 
-    return np.column_stack((macd[0], macd[1], macd[2]))
 
-
-def calc_rsi(closes: np.ndarray, rsi_period: int) -> np.ndarray:
+def calc_rsi(closes: np.ndarray, rsi_period: int) -> pd.Series:
     """Calculate the RSI. Return a 1d numpy array containing the RSI-values."""
-    return talib.RSI(closes * 10000, timeperiod=rsi_period)
+    return pd.Series(talib.RSI(closes * 10000, timeperiod=rsi_period), name=Indicators.RSI)
 
 
-def calc_roc(closes: np.ndarray, roc_period: int) -> np.ndarray:
+def calc_roc(closes: np.ndarray, roc_period: int) -> pd.Series:
     """Calculate the ROC. Return a 1d numpy array containing the ROC-values."""
-    return talib.ROC(closes * 10000, timeperiod=roc_period)
+    return pd.Series(talib.ROC(closes * 10000, timeperiod=roc_period), name=Indicators.ROC)
 
 
 def calc_stoch_rsi(
@@ -98,26 +105,29 @@ def calc_stoch_rsi(
         fastk_period: int,
         fastd_period: int,
         fastd_matype: str
-) -> np.ndarray:
+) -> pd.DataFrame:
     """Calculate the Stochastic RSI. Return a 1d numpy array containing the Stochastic RSI-values."""
     stoch_rsi = talib.STOCHRSI(closes * 10000,
                                timeperiod=stoch_rsi_period,
                                fastk_period=fastk_period,
                                fastd_period=fastd_period,
                                fastd_matype=fastd_matype)
-    return np.column_stack((stoch_rsi[0], stoch_rsi[1]))
+    return pd.DataFrame({
+        Indicators.STOCH_RSI_FASTK: stoch_rsi[0],
+        Indicators.STOCH_RSI_FASTD: stoch_rsi[1]
+    })
 
 
-def calc_will_r(highs: np.ndarray, lows: np.ndarray, closes: np.ndarray, will_r_period: int) -> np.ndarray:
+def calc_will_r(highs: np.ndarray, lows: np.ndarray, closes: np.ndarray, will_r_period: int) -> pd.Series:
     """Calculate the Williams %R. Return a 1d numpy array containing the Williams %R-values."""
-    return talib.WILLR(high=highs, low=lows, close=closes, timeperiod=will_r_period)
+    return pd.Series(talib.WILLR(high=highs, low=lows, close=closes, timeperiod=will_r_period), name=Indicators.WILL_R)
 
 
 # VOLUME INDICATORS
 
-def calc_obv(prices: np.ndarray, volumes: np.ndarray) -> np.ndarray:
+def calc_obv(prices: np.ndarray, volumes: np.ndarray) -> pd.Series:
     """Calculate the On Balance Volume. Return a 1d numpy array containing the OBV-values."""
-    return talib.OBV(prices, volumes)
+    return pd.Series(talib.OBV(prices, volumes), name=Indicators.OBV)
 
 
 # TODO check if this function is still needed
