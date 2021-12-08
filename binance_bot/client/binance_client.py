1d47017d8bda0aded712da14b1dfd624fb3f0790
Georg Unterholzner <georg.unterholzner.coding@gmail.com>
2021-12-08 13:09:02 +0100

    Add constants for column names

--- binance_bot/client/binance_client.py
+++ binance_bot/client/binance_client.py
@@ -1,9 +1,11 @@
 import numpy as np
+import pandas as pd
 import datetime as dt
 
 from binance.client import Client
 
 from binance_bot.configs.credentials import Credentials
+from binance_bot.constants import KlineProps
 
 
 class BinanceClient:
@@ -27,24 +29,37 @@ class BinanceClient:
             quantity=quantity
         )
 
-    def get_historical_data(self, pair: str, interval: str, time_start: str, time_end: str) -> list:
-        return self._client.get_historical_klines(
+    def get_historical_data(self, pair: str, interval: str, time_start: str, time_end: str) -> pd.DataFrame:
+        response = self._client.get_historical_klines(
             symbol=pair,
             interval=interval,
             start_str=time_start,
             end_str=time_end
         )
+        return self._klines_response_to_dataframe(np.array(response))
 
-    def get_klines(self, pair: str, interval: str) -> list:
-        return self._client.get_klines(
-            symbol=pair,
-            interval=interval
-        )
+    def get_klines(self, pair: str, interval: str) -> pd.DataFrame:
+        response = self._client.get_klines(symbol=pair, interval=interval)
+        return self._klines_response_to_dataframe(np.array(response))
 
     @staticmethod
     def api_time_format(ms: int):
-        """Convert a unix timestamp in milliseconds to the
-        date-format required by binance.client."""
+        """Convert a unix timestamp in milliseconds to the date-format required by binance.client."""
         timestamp = dt.datetime.fromtimestamp(ms / 1000)
         time_str = dt.date.strftime(timestamp, '%B %d, %Y %H:%M:%S')
         return time_str
+
+    @staticmethod
+    def _klines_response_to_dataframe(response: np.ndarray) -> pd.DataFrame:
+        """
+        Convert the response from the klines-API to a dataframe.
+        https://binance-docs.github.io/apidocs/spot/en/#kline-candlestick-data
+        """
+        return pd.DataFrame({
+            KlineProps.TIME_OPEN: response[:, 0],
+            KlineProps.OPEN: response[:, 1],
+            KlineProps.HIGH: response[:, 2],
+            KlineProps.LOW: response[:, 3],
+            KlineProps.CLOSE: response[:, 4],
+            KlineProps.VOLUME: response[:, 5]
+        })
