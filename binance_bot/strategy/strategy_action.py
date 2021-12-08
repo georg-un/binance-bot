bc656ddef84f06721dfddf092089c1a5bd35fb0c
Georg Unterholzner <georg.unterholzner.coding@gmail.com>
2021-12-08 22:22:53 +0100

    Fix datatype

--- binance_bot/strategy/strategy_action.py
+++ binance_bot/strategy/strategy_action.py
@@ -1,9 +1,11 @@
 from typing import Literal
 
+import numpy as np
+
 
 class StrategyAction:
 
-    def __init__(self, side: Literal["SELL", "BUY"], pair: str, quantity: float):
+    def __init__(self, side: Literal["SELL", "BUY"], pair: str, quantity: np.double):
         self.side = side
         self.pair = pair
         self.quantity = quantity
