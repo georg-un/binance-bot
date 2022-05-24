f975ecfb8b0112be821222f4578603808bf31434
Georg Unterholzner <georg.unterholzner.coding@gmail.com>
2022-05-24 20:58:45 +0200

    Delete legacy code

--- _old/general/general_modules.py
+++
@@ -1,4 +0,0 @@
-def getlist(option, sep=',', chars=None):
-    """Return a list from a ConfigParser option. By default,
-       split on a comma and strip whitespaces."""
-    return [chunk.strip(chars) for chunk in option.split(sep)]
